f6164efaded70a2c6b59f68b7889ca10eeee6eb8
karankoder <karankr.das.cse22@iitbhu.ac.in>
2024-10-27 02:24:34 +0530

    Implement JWT authentication with cookie management

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
 dotenv.config();
 
 import connectDB from "./config/db.js";
@@ -11,6 +12,8 @@ const port = process.env.BACKEND_PORT;
 
 import userRoutes from "./routes/user.routes.js";
 
+app.use(express.json());
+app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
